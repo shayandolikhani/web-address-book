43a6d345495b0328b81823b11b4e8cd545a008f6
Shayan Dolikhani <43917516+shayandolikhani@users.noreply.github.com>
2021-09-14 23:12:36 -0400

    Second Commit

--- final/render.js
+++ final/render.js
@@ -1,30 +1,47 @@
-// let people;
-// let prev;
-// $(document).ready(function () {
-//     const url = "http://localhost:8080/api/people";
-//     $.ajax({
-//         url: url,
-//         type: "GET",
-//         success: function (data) {
-//             renderContacts(data);
-//         },
-//         error: function (error) {
-//             console.log("Error: " + error);
-//         }
+let people;
+let prev;
+let aToZ = true;
+$(document).ready(function () {
+    const url = "http://localhost:8080/api/people";
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            people = JSON.parse(data).people;
+            people.sort((a, b) => (a.name > b.name) ? 1 : -1);
+            renderContacts(people);
+        },
+        error: function (error) {
+            console.log("Error: " + error);
+        }
 
-//     })
+    })
 
-//     $("#contacts").on('click', ".list-group-item", function () {
-//         let id = $(this).attr("id");
-//         displayContact(id);
-//     })
-// })
+    $("#contacts").on('click', ".list-group-item", function () {
+        let id = $(this).attr("id");
+        displayContact(id);
+    })
 
-function renderContacts(data) {
-    people = JSON.parse(data).people;
-    people.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    $("#contacts").on('click', ".btn", function () {
+        if (aToZ) {
+            people.sort((a, b) => (b.name > a.name) ? 1 : -1);
+            aToZ = false;
+        } else {
+            people.sort((a, b) => (a.name > b.name) ? 1 : -1);
+            aToZ = true;
+        }
+        
+        renderContacts(people);
+    })
+})
+
+function renderContacts(people) {
+    $("#contacts").empty();
     let letter = people[0].name[0];
-    $("#contacts").append(`<h1>${letter}</h1>`);
+    $("#contacts").append(` <div class = "sortflex">
+                                <h1>${letter}</h1>
+                                <button class="btn btn-primary" type="button"><span class = "arrow">↓</span></button>
+                            </div>`);
     for (let i = 0; i < people.length; i++) {
         if (people[i].name[0] !== letter) {
             letter = people[i].name[0];
